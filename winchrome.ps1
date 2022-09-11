e99638e1c8413a6a6d41f4b11a59334cb9b43a20
Nirav Kamdar <nirav.c.kamdar@oracle.com>
2022-09-12 03:35:08 +1000

    Updated the file

--- winchrome.ps1
+++ winchrome.ps1
@@ -1,8 +1,8 @@
 winrm quickconfig -force;
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-winrm set winrm/config/service/auth '@{Basic="true"}'
 Set-NetConnectionProfile -Name "Network" -NetworkCategory Private;
 netsh advfirewall firewall add rule name=WinRM-HTTP dir=in localport=5985 protocol=TCP action=allow;
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
 $Installer = $env:TEMP + "\chrome_installer.exe"; 
 Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Installer; 
 Start-Process -FilePath $Installer -Args "/silent /install" -Verb RunAs -Wait; 
