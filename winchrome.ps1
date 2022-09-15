9205ce9226f2cefb4631cc418585130fc9f4f07a
Nirav Kamdar <nirav.c.kamdar@oracle.com>
2022-09-15 18:30:43 +1000

    Update winchrome.ps1

--- winchrome.ps1
+++ winchrome.ps1
@@ -3,6 +3,7 @@ Set-NetConnectionProfile -Name "Network" -NetworkCategory Private;
 netsh advfirewall firewall add rule name=WinRM-HTTP dir=in localport=5985 protocol=TCP action=allow;
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
+powershell
 $sharepath = "C:\Users\azureuser\AppData\Local\Temp"
 $Acl = Get-ACL $SharePath
 $AccessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("everyone","FullControl","ContainerInherit,Objectinherit","none","Allow")
