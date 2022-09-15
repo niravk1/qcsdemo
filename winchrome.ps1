7589f84de26f58c3037fff1d67cd0297f5f10a28
Nirav Kamdar <nirav.c.kamdar@oracle.com>
2022-09-15 19:04:08 +1000

    Update winchrome.ps1

--- winchrome.ps1
+++ winchrome.ps1
@@ -3,14 +3,13 @@ Set-NetConnectionProfile -Name "Network" -NetworkCategory Private;
 netsh advfirewall firewall add rule name=WinRM-HTTP dir=in localport=5985 protocol=TCP action=allow;
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
-powershell
 mkdir C:\Qualys
-C:\Users\azureuser\Downloads
+cd C:\Users\azureuser\Downloads
 Invoke-WebRequest -OutFile "Firefox Installer.exe" -Uri https://download-installer.cdn.mozilla.net/pub/firefox/releases/101.0.1/win64/en-US/Firefox%20Setup%20101.0.1.exe
 Start-Process -Wait -FilePath "C:\Users\azureuser\Downloads\Firefox Installer.exe" -ArgumentList "/S" -PassThru
 $sharepath = "C:\Users\azureuser\AppData\Local\Temp"
 $Acl = Get-ACL $SharePath
-$AccessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("everyone","FullControl","ContainerInherit,Objectinherit","none","Allow")
+$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("everyone","FullControl","ContainerInherit,Objectinherit","none","Allow")
 $Acl.AddAccessRule($AccessRule)
 Set-Acl $SharePath $Acl
 $Installer = $env:TEMP + "\chrome_installer.exe"; 
