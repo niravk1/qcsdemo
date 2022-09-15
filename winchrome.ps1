030d5073730b58975ba29405b707e2b4a415e464
Nirav Kamdar <nirav.c.kamdar@oracle.com>
2022-09-15 19:15:25 +1000

    Update winchrome.ps1

--- winchrome.ps1
+++ winchrome.ps1
@@ -4,15 +4,16 @@ netsh advfirewall firewall add rule name=WinRM-HTTP dir=in localport=5985 protoc
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 mkdir C:\Qualys
-cd C:\Users\azureuser\Downloads
-Invoke-WebRequest -OutFile "Firefox Installer.exe" -Uri https://download-installer.cdn.mozilla.net/pub/firefox/releases/101.0.1/win64/en-US/Firefox%20Setup%20101.0.1.exe
-Start-Process -Wait -FilePath "C:\Users\azureuser\Downloads\Firefox Installer.exe" -ArgumentList "/S" -PassThru
+Invoke-WebRequest -OutFile "FirefoxInstaller.exe" -Uri https://download-installer.cdn.mozilla.net/pub/firefox/releases/101.0.1/win64/en-US/Firefox%20Setup%20101.0.1.exe
+Start-Process -Wait -FilePath "C:\Qualys\FirefoxInstaller.exe" -ArgumentList "/S" -PassThru
+Invoke-WebRequest -OutFile "ChromeStandaloneSetup.exe" -Uri https://www.slimjet.com/chrome/download-chrome.php?file=files%2F104.0.5112.102%2FChromeStandaloneSetup.exe
+Start-Process -Wait -FilePath "C:\Qualys\ChromeStandaloneSetup.exe" -ArgumentList "/S" -PassThru
 $sharepath = "C:\Users\azureuser\AppData\Local\Temp"
 $Acl = Get-ACL $SharePath
 $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("everyone","FullControl","ContainerInherit,Objectinherit","none","Allow")
 $Acl.AddAccessRule($AccessRule)
 Set-Acl $SharePath $Acl
-$Installer = $env:TEMP + "\chrome_installer.exe"; 
-Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Installer; 
-Start-Process -FilePath $Installer -Args "/silent /install" -Verb RunAs -Wait; 
-Remove-Item $Installer;
+#$Installer = $env:TEMP + "\chrome_installer.exe"; 
+#Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Installer; 
+#Start-Process -FilePath $Installer -Args "/silent /install" -Verb RunAs -Wait; 
+#Remove-Item $Installer;
