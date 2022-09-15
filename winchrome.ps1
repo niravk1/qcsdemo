983e462964f18ff55e312c0893e7b2bcb9fb0f1a
Nirav Kamdar <nirav.c.kamdar@oracle.com>
2022-09-15 12:49:13 +1000

    Update winchrome.ps1

--- winchrome.ps1
+++ winchrome.ps1
@@ -3,6 +3,11 @@ Set-NetConnectionProfile -Name "Network" -NetworkCategory Private;
 netsh advfirewall firewall add rule name=WinRM-HTTP dir=in localport=5985 protocol=TCP action=allow;
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
+$sharepath = "C:\Users\azureuser\AppData\Local\Temp"
+$Acl = Get-ACL $SharePath
+$ccessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("everyone","FullControl","ContainerInherit,Objectinherit","none","Allow")
+$Acl.AddAccessRule($AccessRule)
+Set-Acl $SharePath $Acl
 $Installer = $env:TEMP + "\chrome_installer.exe"; 
 Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Installer; 
 Start-Process -FilePath $Installer -Args "/silent /install" -Verb RunAs -Wait; 
