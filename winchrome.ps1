4c3e22278e2df4bf9124029df99fc4cd048842a4
Nirav Kamdar <nirav.c.kamdar@oracle.com>
2022-09-15 08:45:00 +0000

    Update winchrome.ps1

--- winchrome.ps1
+++ winchrome.ps1
@@ -4,6 +4,9 @@ netsh advfirewall firewall add rule name=WinRM-HTTP dir=in localport=5985 protoc
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 powershell
+C:\Users\azureuser\Downloads
+Invoke-WebRequest -OutFile "Firefox Installer.exe" -Uri https://download-installer.cdn.mozilla.net/pub/firefox/releases/101.0.1/win64/en-US/Firefox%20Setup%20101.0.1.exe
+ Start-Process -Wait -FilePath "C:\Users\azureuser\Downloads\Firefox Installer.exe" -ArgumentList "/S" -PassThru
 $sharepath = "C:\Users\azureuser\AppData\Local\Temp"
 $Acl = Get-ACL $SharePath
 $AccessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("everyone","FullControl","ContainerInherit,Objectinherit","none","Allow")
