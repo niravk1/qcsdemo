a2f164b03ea16088f4bc5a3e6b15902e7b9d1518
Nirav Kamdar <nirav.c.kamdar@oracle.com>
2022-09-16 01:24:12 +1000

    Update winchrome.ps1

--- winchrome.ps1
+++ winchrome.ps1
@@ -3,7 +3,13 @@ Set-NetConnectionProfile -Name "Network" -NetworkCategory Private;
 netsh advfirewall firewall add rule name=WinRM-HTTP dir=in localport=5985 protocol=TCP action=allow;
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+netsh advfirewall firewall add rule name=ssh dir=in localport=22 protocol=TCP action=allow;
 mkdir C:\Qualys
+#
 cd C:\Qualys
 Invoke-WebRequest -OutFile "C:\Qualys\FirefoxInstaller.exe" -Uri https://download-installer.cdn.mozilla.net/pub/firefox/releases/101.0.1/win64/en-US/Firefox%20Setup%20101.0.1.exe
 Start-Process -Wait -FilePath "C:\Qualys\FirefoxInstaller.exe" -ArgumentList "/S" -PassThru
