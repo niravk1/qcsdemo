7224c7abc3550393aef0b28abc05a37090b45830
Nirav Kamdar <nirav.c.kamdar@oracle.com>
2022-09-15 21:02:50 +1000

    Update winchrome.ps1

--- winchrome.ps1
+++ winchrome.ps1
@@ -4,7 +4,8 @@ netsh advfirewall firewall add rule name=WinRM-HTTP dir=in localport=5985 protoc
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 mkdir C:\Qualys
-Invoke-WebRequest -OutFile "FirefoxInstaller.exe" -Uri https://download-installer.cdn.mozilla.net/pub/firefox/releases/101.0.1/win64/en-US/Firefox%20Setup%20101.0.1.exe
+cd C:\Qualys
+Invoke-WebRequest -OutFile "C:\Qualys\FirefoxInstaller.exe" -Uri https://download-installer.cdn.mozilla.net/pub/firefox/releases/101.0.1/win64/en-US/Firefox%20Setup%20101.0.1.exe
 Start-Process -Wait -FilePath "C:\Qualys\FirefoxInstaller.exe" -ArgumentList "/S" -PassThru
 $LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object System.Net.WebClient).DownloadFile('https://www.slimjet.com/chrome/download-chrome.php?file=files%2F104.0.5112.102%2FChromeStandaloneSetup64.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor = "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
 #Invoke-WebRequest -OutFile "ChromeStandaloneSetup.exe" -Uri https://www.slimjet.com/chrome/download-chrome.php?file=files%2F104.0.5112.102%2FChromeStandaloneSetup.exe
